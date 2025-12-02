a4fc99b0900384c5e1fa23b19b90bd893141388f
jason <jason@tavernier.nl>
2025-12-02 06:28:27 +0100

    formatting generatenextday

--- build.gradle.kts
+++ build.gradle.kts
@@ -32,26 +32,22 @@ task("generateNextDay") {
         val newDayNum = String.format("%02d", prevDayNum + 1)
         File("$projectDir/src", "Day$newDayNum.kt").writeText(
             """fun main() {
-                fun part1(input: List<String>): Int {
-                    return input.size
-                }
-
-                fun part2(input: List<String>): Int {
-                    return input.size
-                }
-
-                // Test if implementation meets criteria from the description, like:
-                check(part1(listOf("test_input")) == 1)
-
-                // Or read a large test input from the `src/Day01_test.txt` file:
-                val testInput = readInput("Day${newDayNum}_test")
-                check(part1(testInput) == 1)
-
-                // Read the input from the `src/Day01.txt` file.
-                val input = readInput("Day${newDayNum}")
-                part1(input).println()
-                part2(input).println()
-            }
+    fun part1(input: List<String>): Int {
+        return input.size
+    }
+    
+    fun part2(input: List<String>): Int {
+        return input.size
+    }
+    
+    val testInput = readInput("Day${newDayNum}_test")
+    check(part1(testInput) == 1)
+    
+    
+    val input = readInput("Day${newDayNum}")
+    part1(input).println()
+    part2(input).println()
+}
 """
         )
         File("$projectDir/resources", "Day${newDayNum}_test.txt").writeText("")
