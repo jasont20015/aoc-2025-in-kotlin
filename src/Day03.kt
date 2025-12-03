c6d5a53edcc620b5d5c95c5ee026270ca3e01990
jason <jason@tavernier.nl>
2025-12-03 06:43:15 +0100

    day 3

--- src/Day03.kt
+++ src/Day03.kt
@@ -1,14 +1,69 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        var total = 0
+        var highestNum: Int
+        var secondHighest: Int
+        for (line in input){
+            var lineAsArr = mutableListOf<Int>()
+            for(c in line){
+                val num = c.digitToInt()
+                lineAsArr.add(num)
+            }
+            highestNum = lineAsArr.max()
+            if(lineAsArr.indexOf(highestNum) == lineAsArr.size - 1) {
+                lineAsArr.removeLast()
+                secondHighest = lineAsArr.max()
+                total += (secondHighest * 10) + highestNum
+            }else{
+                while(lineAsArr.first() != highestNum) {
+                    lineAsArr.removeFirst()
+                }
+                lineAsArr.removeFirst()
+                secondHighest = lineAsArr.max()
+                total += (highestNum * 10) + secondHighest
+            }
+        }
+
+        return total
     }
-    
-    fun part2(input: List<String>): Int {
-        return input.size
+
+
+
+    fun part2(input: List<String>): Long {
+        var total = 0L
+        for (line in input) {
+            val choices = mutableListOf<Int>()
+            val lineAsArr = mutableListOf<Int>()
+            for (c in line) {
+                val num = c.digitToInt()
+                lineAsArr.add(num)
+            }
+            var start = 0
+            var choiceNumber = 0
+            while(choiceNumber <  12){
+                val end = lineAsArr.size - (12 - choices.size)
+                var currentMax = 0
+                var maxIndex = -1
+                for (i in start..end){
+                    val digit = lineAsArr[i]
+                    if(digit > currentMax){
+                        currentMax = digit
+                        maxIndex = i
+                    }
+                }
+                choices.add(currentMax)
+                start = maxIndex+1
+                choiceNumber++
+            }
+            val joined = (choices.joinToString("")).toLong()
+            total += joined
+
+        }
+        return total
     }
     
     val testInput = readInput("Day03_test")
-    check(part1(testInput) == 1)
+    check(part2(testInput) == 3121910778619L)
     
     
     val input = readInput("Day03")
